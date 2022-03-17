9f2da4ceacec8c74279e2d00691502dc8f726a76
Johannes Bechberger <johannes.bechberger@sap.com>
2022-03-17 15:47:07 +0100

    Improve build and run script

--- run.sh
+++ run.sh
@@ -1,14 +1,6 @@
 #! /bin/sh
 
-case "$(uname -s)" in
-   Darwin)
-     SCRIPT_PATH=$(greadlink -f "$0")
-     ;;
-
-   *)
-     SCRIPT_PATH=$(readlink -f "$0")
-     ;;
-esac
+SCRIPT_PATH=$(readlink -f "$0")
 
 cd $(dirname "$SCRIPT_PATH")
 
