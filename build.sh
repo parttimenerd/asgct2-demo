141a5fa13ab51e2f8737ab22373427dc9798b842
Johannes Bechberger <johannes.bechberger@sap.com>
2022-07-28 17:43:59 +0200

    Bump JDK repo

--- build.sh
+++ build.sh
@@ -4,6 +4,6 @@ SCRIPT_PATH=$(readlink -f "$0")
 
 cd $(dirname "$SCRIPT_PATH")
 
-(cd jdk; bash configure; make images)
+(cd jdk; bash configure --disable-warnings-as-errors; make images)
 (cd async-profiler; make)
 
